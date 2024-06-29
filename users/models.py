aad91e9901c288fc2a9eebec898f93db058798f4
К <hitandrun444@yandex.ru>
2024-06-29 11:30:03 +0300

    DRF24.1-Models_add_classUser

--- users/models.py
+++ users/models.py
@@ -1,5 +1,40 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name='почта',
+        help_text='укажите почту'
+    )
+    phone = models.CharField(
+        max_length=40,
+        blank=True,
+        null=True,
+        verbose_name='телефон',
+        help_text='укажите телефон'
+    )
+    city = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='город',
+        help_text='укажите свой город'
+    )
+    avatar = models.ImageField(
+        upload_to='users/avatars',
+        blank=True,
+        null=True,
+        verbose_name='аватар',
+        help_text='загрузите свой аватар'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь',
+        verbose_name_plural = 'Пользователи'
